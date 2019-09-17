45b71ad0a8c6fe2b01c5316f25f09389f303c0b2
MrYTF <1534171551@qq.com>
2019-09-17 13:43:12 +0800

    this is a demo!

--- demo/src/main/java/com/example/demo/model/Emp.java
+++ demo/src/main/java/com/example/demo/model/Emp.java
@@ -1,8 +1,11 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Emp {
     private String name;
     private int age;
     private double sal;
     private int delete;
+    private Date date;
 }
